853e5cb8e7b5684a5b35e880cae2eb96baba3be7
AkshaySharma008 <akshaysharma30121999@gmail.com>
2021-04-24 17:44:32 +0530

    add auth guard

--- apps/dashboard/src/app/app-routing.module.ts
+++ apps/dashboard/src/app/app-routing.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 
-// const routes: Routes = [
-//     { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-//     { path: 'projects', canActivate: [AuthGuard], loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
-//     { path: 'questions', canActivate: [AuthGuard], loadChildren: () => import('./questions/questions.module').then(m => m.QuestionsModule) },
-//     { path: 'quiz', canActivate: [AuthGuard], loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) },
-//     { path: 'exam', canActivate: [AuthGuard], loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) }
-    
-// ];
-
-
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'projects',  loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
-  { path: 'questions',  loadChildren: () => import('./questions/questions.module').then(m => m.QuestionsModule) },
-  { path: 'quiz',  loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) },
-  { path: 'exam',  loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) }
-  
+    { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+    { path: 'projects', canActivate: [AuthGuard], loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
+    { path: 'questions', canActivate: [AuthGuard], loadChildren: () => import('./questions/questions.module').then(m => m.QuestionsModule) },
+    { path: 'quiz', canActivate: [AuthGuard], loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) },
+    { path: 'exam', canActivate: [AuthGuard], loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) }
+    
 ];
+
+
+// const routes: Routes = [
+//   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+//   { path: 'projects',  loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
+//   { path: 'questions',  loadChildren: () => import('./questions/questions.module').then(m => m.QuestionsModule) },
+//   { path: 'quiz',  loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) },
+//   { path: 'exam',  loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) }
+  
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
